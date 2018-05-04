b3c878c134bf852da850ec4c02e9dbc29978200f
hemstrow <whemstrom@ucdavis.edu>
2018-05-04 14:32:17 -0700

    Better creation of 2pop models. New parmfile with 50 runs of each model type, 20 iterations.

--- scripts/dadi/make_dadi_parmfile.R
+++ scripts/dadi/make_dadi_parmfile.R
@@ -94,29 +94,56 @@ m21 <- c(.1, 0, 5)
 r1 <- c(1, .01, 4)
 r2 <- c(1, .01, 4)
 
+#how many iters?
+iters <- 20
+
 #which models and how many reps?
-mods <- c(rep("p2_cgrowth", 20),
-          rep("p2_lgrowth_both", 20),
-          rep("p2_lgrowth_1", 20),
-          rep("p2_lgrowth_2", 20))
+reps_per_perm <- 50
+mods <- c("p2_cgrowth", "p2_lgrowth_both", "p2_lgrowth_1", "p2_lgrowth_2")
 
-#how many iterations?
-maxiters <- c(rep(10, length(mods)))
-
-#which pops?
-pops <- c(rep(c(rep("[HAW,GUA]", 10), rep("[GUA,HAW]", 10)), 4))
+#which pops to use?
+pops <- c("[HAW,GUA]", "[HAW,ROT]", "[GUA,HAW]", "[ROT,HAW]")
 
 #fold the spectra?
-fs <- rep("False", length(mods))
+fs <- "False"
 
 #fold initial params?
-fp <- rep(3, length(mods))
+fp <- 3
 
 #what projection?
-proj <- rep("[15,15]", length(mods))
+proj <- "[15,15]"
+
+#which optimizer?
+optim <- "fmin"
+
+#outfile?
+ofile <- "dadi/parmfiles/1st_passHGR_2d_parms.txt"
+
+##########################################
+#make the parm df
+#models
+rpp <- reps_per_perm
+rppm <- rpp*length(pops)
+mods <- sort(rep(mods, rppm))
+
+#how many iterations?
+maxiters <- rep(iters, length(mods))
+
+#which pops?
+rppp <- rpp*length(unique(mods))
+pops <- rep(pops, rppp)
+
+#fold the spectra?
+fs <- rep(fs, length(mods))
+
+#fold initial params?
+fp <- rep(fp, length(mods))
+
+#what projection?
+proj <- rep(proj, length(mods))
 
 #what optimizer?
-optimizer <- rep("fmin", length(mods))
+optim <- rep(optim, length(mods))
 
 
 ##set ip, lb, ub
@@ -125,47 +152,27 @@ lb <- ip
 ub <- ip
 for(i in 1:length(ip)){
   if(mods[i] == "p2_cgrowth"){
-    tip <- paste0(c(nu1B[1], nu2B[1], nu1f[1], nu2f[1], ts[1], tp[1], m12[1], m21[1]), collapse = ",")
-    tlb <- paste0(c(nu1B[2], nu2B[2], nu1f[2], nu2f[2], ts[2], tp[2], m12[2], m21[2]), collapse = ",")
-    tub <- paste0(c(nu1B[3], nu2B[3], nu1f[3], nu2f[3], ts[3], tp[3], m12[3], m21[3]), collapse = ",")
+    tparms <- cbind(nu1B, nu2B, nu1f, nu2f, ts, tp, m12, m21)
   }
   else if (mods[i] == "p2_lgrowth_both"){
-    tip <- paste0(c(nu1B[1], nu2B[1], K1[1], K2[1], ts[1], tp[1], m12[1], m21[1], r1[1], r2[1]), collapse = ",")
-    tlb <- paste0(c(nu1B[2], nu2B[2], K1[2], K2[2], ts[2], tp[2], m12[2], m21[2], r1[2], r2[2]), collapse = ",")
-    tub <- paste0(c(nu1B[3], nu2B[3], K1[3], K2[3], ts[3], tp[3], m12[3], m21[3], r1[3], r2[3]), collapse = ",")
+    tparms <- cbind(nu1B, nu2B, K1, K2, ts, tp, m12, m21, r1, r2)
   }
   else if (mods[i] == "p2_lgrowth_1"){
-    tip <- paste0(c(nu1B[1], nu2B[1], K1[1], nu2f[1], ts[1], tp[1], m12[1], m21[1], r1[1]), collapse = ",")
-    tlb <- paste0(c(nu1B[2], nu2B[2], K1[2], nu2f[2], ts[2], tp[2], m12[2], m21[2], r1[2]), collapse = ",")
-    tub <- paste0(c(nu1B[3], nu2B[3], K1[3], nu2f[3], ts[3], tp[3], m12[3], m21[3], r1[3]), collapse = ",")
+    tparms <- cbind(nu1B, nu2B, K1, nu2f, ts, tp, m12, m21, r1)
   }
   else if (mods[i] == "p2_lgrowth_2"){
-    tip <- paste0(c(nu1B[1], nu2B[1], nu1f[1], K2[1], ts[1], tp[1], m12[1], m21[1], r2[1]), collapse = ",")
-    tlb <- paste0(c(nu1B[2], nu2B[2], nu1f[2], K2[2], ts[2], tp[2], m12[2], m21[2], r2[2]), collapse = ",")
-    tub <- paste0(c(nu1B[3], nu2B[3], nu1f[3], K2[3], ts[3], tp[3], m12[3], m21[3], r2[3]), collapse = ",")
+    tparms <- cbind(nu1B, nu2B, nu1f, K2, ts, tp, m12, m21, r2)
   }
-  ip[i] <- paste0("[", tip, "]")
-  lb[i] <- paste0("[", tlb, "]")
-  ub[i] <- paste0("[", tub, "]")
+  ip[i] <- paste0("[", paste0(tparms[1,], collapse = ","), "]")
+  lb[i] <- paste0("[", paste0(tparms[2,], collapse = ","), "]")
+  ub[i] <- paste0("[", paste0(tparms[3,], collapse = ","), "]")
 }
 
 
-parms3 <- cbind(mods, maxiters, pops, fs, fp, ip, ub, lb, proj, optimizer)
+#combine
+parms <- cbind(mods, maxiters, pops, fs, fp, ip, ub, lb, proj, optim)
 
-#add to rota models
-parms4 <- gsub("GUA", "ROT", parms3)
-
-
-
+#write
+write.table(parms, ofile, quote = F, sep = " ", row.names = F, col.names = F)
 
 
-
-
-
-
-
-
-#############combine#############
-parms <- rbind(parms, parms2, parms3, parms4)
-
-write.table(parms, "dadi/parmfiles/1st_folded_optim_snps_both_mods.txt", quote = F, sep = " ", row.names = F, col.names = F)

ee4384c2e04f0dcfade75a0d804ae64f00fa1624
hemstrow <whemstrom@ucdavis.edu>
2019-04-12 15:42:59 -0700

    added the sliding window fst script using angsd

---
+++ sliding_window_FST/calc_sliding_window.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -t 6-24:00:00
+#SBATCH --mem=20G
+#SBATCH -J ENA_WNA_fst
+
+# copy this script into a new directory under monarchs
+# edit the paths for angsd and realSFS. RealSFS is in the angsd/misc folder
+# edit the paths to the bamlists, they are in the Raw_data folder of the github repo
+# don't run this in the github, run it in the general monarch folder
+# angsd is in ~mlyjones/bin/angsd_dir
+
+# to run this, type sbatch calc_sliding_window.sh
+
+
+
+#this is with 2pops
+#first calculate per pop saf for each population
+~/angsd/angsd -b ../Raw_data/ENA_bamlist.txt -anc /home/hemstrow/genomes/Dp_genome_v3.fasta -out ENA -dosaf 1 -gl 1 -fold 1
+~/angsd/angsd -b ../Raw_data/WNA_bamlist.txt -anc /home/hemstrow/genomes/Dp_genome_v3.fasta -out WNA -dosaf 1 -gl 1 -fold 1
+
+#calculate the 2dsfs prior
+ll ~/angsd/misc/realSFS ENA.saf.idx WNA.saf.idx > ENA.WNA.ml
+
+#prepare the fst for easy window analysis etc
+../misc/realSFS fst index ENA.saf.idx WNA.saf.idx -sfs ENA.WNA.ml -fstout ENA.WNA
+
+#get the global estimate
+../misc/realSFS fst stats ENA.WNA.fst.idx > ENA_WNA_global_fst.txt
+
+#below is not tested that much, but seems to work
+../misc/realSFS fst stats2 ENA.WNA.fst.idx -win 50000 -step 10000 > ENA_WNA_sliding_window.txt

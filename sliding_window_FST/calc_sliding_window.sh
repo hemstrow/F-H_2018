20d6cc6fa944b20d04e61875570f3bed26ed14be
hemstrow <whemstrom@ucdavis.edu>
2019-05-02 11:36:51 -0700

    updated the sliding window script to take file paths as variables for easier editing

--- sliding_window_FST/calc_sliding_window.sh
+++ sliding_window_FST/calc_sliding_window.sh
@@ -11,21 +11,22 @@
 
 # to run this, type sbatch calc_sliding_window.sh
 
-
+angsdpath=~/angsd/angsd
+sfspath=~/angsd/misc/realSFS
 
 #this is with 2pops
 #first calculate per pop saf for each population
-~/angsd/angsd -b ../Raw_data/ENA_bamlist.txt -anc /home/hemstrow/genomes/Dp_genome_v3.fasta -out ENA -dosaf 1 -gl 1 -fold 1
-~/angsd/angsd -b ../Raw_data/WNA_bamlist.txt -anc /home/hemstrow/genomes/Dp_genome_v3.fasta -out WNA -dosaf 1 -gl 1 -fold 1
+${angsdpath} -b ../Raw_data/ENA_bamlist.txt -anc /home/hemstrow/genomes/Dp_genome_v3.fasta -out ENA -dosaf 1 -gl 1 -fold 1
+${angsdpath} -b ../Raw_data/WNA_bamlist.txt -anc /home/hemstrow/genomes/Dp_genome_v3.fasta -out WNA -dosaf 1 -gl 1 -fold 1
 
 #calculate the 2dsfs prior
-ll ~/angsd/misc/realSFS ENA.saf.idx WNA.saf.idx > ENA.WNA.ml
+ll ${sfspath} ENA.saf.idx WNA.saf.idx > ENA.WNA.ml
 
 #prepare the fst for easy window analysis etc
-../misc/realSFS fst index ENA.saf.idx WNA.saf.idx -sfs ENA.WNA.ml -fstout ENA.WNA
+${sfspath} fst index ENA.saf.idx WNA.saf.idx -sfs ENA.WNA.ml -fstout ENA.WNA
 
 #get the global estimate
-../misc/realSFS fst stats ENA.WNA.fst.idx > ENA_WNA_global_fst.txt
+${sfspath} fst stats ENA.WNA.fst.idx > ENA_WNA_global_fst.txt
 
 #below is not tested that much, but seems to work
-../misc/realSFS fst stats2 ENA.WNA.fst.idx -win 50000 -step 10000 > ENA_WNA_sliding_window.txt
+${sfspath} fst stats2 ENA.WNA.fst.idx -win 50000 -step 10000 > ENA_WNA_sliding_window.txt

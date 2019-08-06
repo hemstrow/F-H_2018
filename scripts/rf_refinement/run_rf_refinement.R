2ed482ec1fa2b2a5f81521c4355bcb478b9c6689
hemstrow <whemstrom@ucdavis.edu>
2019-08-06 12:21:32 -0700

    updated the refinement script to just run one refinement and stop, hopefully less overfitting.

--- scripts/rf_refinement/run_rf_refinement.R
+++ scripts/rf_refinement/run_rf_refinement.R
@@ -1,3 +1,12 @@
+# run parameters
+response <- "migration_distance"
+mtry.scale <- 1 # percentage of snps to use in mtry
+num.trees <- 50000 # number of trees in initial run. Probably doesn't need to be huge!
+par <- F
+trim <- .9
+
+
+
 # define paths, library snpR
 .libPaths(c(.libPaths(), "/home/hemstrow/R/x86_64-pc-linux-gnu-library/3.6", "/usr/local/lib/R/site-library", "/usr/lib/R/site-library", "/usr/lib/R/library", "/share/apps/rmodules"))
 library(snpR); library(ranger);
@@ -50,42 +59,30 @@ dat <- filter_snps(dat, 0.05, 0.55)
 ## remove the cross sample
 rdat <- subset_snpR_data(dat, samps = (1:ncol(dat))[-cross_sample])
 
-# run parameters
-trim_cuttoffs <- c(1000, 200) # above the first level, will trim at the first percentage, below the last, will trim a single at a time
-trim <- c(.9, .1) # trim at .9 before 1000 SNPs, at .1 before 200, and one per run below this.
-response <- "migration_distance"
-num.trees <- 50000 # number of trees
-init.mtry <- nrow(rdat) # initial mtry
-init.num.trees <- 50000 # number of trees in initial run. Probably doesn't need to be huge!
-par <- 11
-
 # run the first random forest
-rf <- run_random_forest(rdat, response = response, mtry = init.mtry, num.trees = init.num.trees)
+rf <- run_random_forest(rdat, response = response, mtry = nrow(rdat)*mtry.scale, num.trees = num.trees, par = par)
 
 # run the refinement
-refined_model <- refine_rf(rf = rf, 
-                           response = response, 
-                           trim_cuttoffs = trim_cuttoffs, 
-                           num.trees = num.trees,
-                           trim = trim,
-                           par = par)
+best.imp <- quantile(abs(rf$models$.base_.base$model$variable.importance), trim)
+best.imp <- which(rf$models$.base_.base$model$variable.importance >= best.imp[1])
+rdat <- subset_snpR_data(dat, best.imp)
+rf <- run_random_forest(rdat, response = response, mtry = nrow(rdat)*mtry.scale, num.trees = num.trees, 
+                        par = par, importance = "permutation", pvals = F)
+
+
 
 # save outputs
-error.delta <- refined_model$error_delta
-error.delta <- cbind(run = run, error.delta)
-write.table(error.delta, paste0(outfile, "_delta_", run, ".txt"), quote = F, row.names = F, col.names = F)
-saveRDS(refined_model, paste0(outfile, "_model_", run, ".RDS"))
+aveRDS(refined_model, paste0(outfile, "_model_", run, ".RDS"))
 
-# prediction, need to re-run the model since the impurity_corrected importance can apparently cause issues. Run with permutation.
-kept.snps <- refined_model$best_model$data@snp.meta$.snp.id
+## prediction, need to re-run the model since the impurity_corrected importance can apparently cause issues. Run with permutation.
+kept.snps <- rf$data@snp.meta$.snp.id
 kept.snps <- match(kept.snps, dat@snp.meta$.snp.id)
 
-predict.rf <- run_random_forest(refined_model$best_model$data, response = response, mtry = nrow(refined_model$best_model$data), num.trees = num.trees, importance = "permutation", pvals = F)
 sn <- format_snps(dat, "sn")
 sn <- sn[,-c(1:3)]
 sn <- sn[kept.snps, cross_sample]
 sn <- as.data.frame(t(sn))
-colnames(sn) <- refined_model$best_model$models$.base_.base$model$forest$independent.variable.names
+colnames(sn) <- rf$models$.base_.base$model$forest$independent.variable.names
 out <- predict(predict.rf$models$.base_.base$model, sn)
 
 write.table(data.frame(sample = cross_sample, phenotype = dat@sample.meta[cross_sample,response], prediction = out$predictions),

cf7c3d964503dd0ae82c475c4c9d55d038de2ecd
hemstrow <whemstrom@ucdavis.edu>
2019-08-26 14:01:31 -0700

    Did a few other directionality comparisons

--- scripts/directionality_and_sfs_plots.R
+++ scripts/directionality_and_sfs_plots.R
@@ -12,24 +12,42 @@ x <- read.table("data/dadi_inputs/dadi_10kgap_snps.txt", header = T, stringsAsFa
 # NAM, HAW
 sf <- make_SFS(x, c("NAM", "HAW"), c(100, 10), F)
 plot_sfs(sf)
-calc_directionality(sf)
+NH <- calc_directionality(sf)
 
 # HAW, GUA
 sf <- make_SFS(x, c("HAW", "GUA"), c(10, 15), F)
 plot_sfs(sf)
-calc_directionality(sf)
+HG <- calc_directionality(sf)
 
 # GUA, ROT
 sf <- make_SFS(x, c("GUA", "ROT"), c(15, 15), F)
 plot_sfs(sf)
-calc_directionality(sf)
+GR <- calc_directionality(sf)
 
 # HAW, QLD
 sf <- make_SFS(x, c("HAW", "QLD"), c(10, 10), F)
 plot_sfs(sf)
-calc_directionality(sf)
+HQ <- calc_directionality(sf)
 
 # HAW, NOR
 sf <- make_SFS(x, c("HAW", "NOR"), c(10, 10), F)
 plot_sfs(sf)
-calc_directionality(sf)
+HN <- calc_directionality(sf)
+
+
+
+# make a table of the comparisons of interest
+dirs <- as.data.frame(t(combn(c("NAM", "HAW", "GUA", "QLD"), 2)), stringsAsFactors = F)
+colnames(dirs)[1:2] <- c("pop1", "pop2")
+dirs$di <- 0
+dirs$direction <- ""
+proj.sizes <- c(100, 10, 15, 20)
+names(proj.sizes) <- c("NAM", "HAW", "GUA", "QLD")
+for(i in 1:nrow(dirs)){
+  print(dirs[i,1:2])
+  sf <- make_SFS(x, c(dirs[i,1], dirs[i,2]), proj.sizes[match(dirs[i,1:2], names(proj.sizes))])
+  di <- calc_directionality(sf)
+  dirs$di[i] <- di
+  dirs$direction[i] <- attr(di, "direction")
+}
+
